479e9364acb1104f7bb13ca7b77d3c77ecd21ea2
Cano <canwoahzo@gmail.com>
2022-12-11 18:27:34 -0600

    2022 day 01

---
+++ src/event22/day01.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{prelude::*, self};
+use aoc_rust::answer::Answer;
+
+pub fn solve() -> Result<Answer, io::Error> {
+    let mut puzzle = String::new();
+
+    File::open("inputs/event22/day01/input.txt")?
+        .read_to_string(&mut puzzle)?;
+
+    let mut elfs_calories: Vec<isize> = Vec::new();
+    let mut calories_count: isize = 0;
+
+    for calorie_or_nothing in puzzle.lines() {
+        if calorie_or_nothing.is_empty() {
+            elfs_calories.push(calories_count);
+            calories_count = 0;
+        } else {
+            let calories: isize = calorie_or_nothing.parse().unwrap();
+            calories_count += calories;
+        }
+    }
+
+    elfs_calories.sort_by(|a, b| b.cmp(a));
+
+    let answer_p1 = elfs_calories[0];
+    let answer_p2 = elfs_calories[0] + elfs_calories[1] + elfs_calories[2];
+
+    Ok(Answer {
+        event: String::from("2022"),
+        day: String::from("01"),
+        part1: answer_p1.to_string(),
+        part2: answer_p2.to_string(),
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod event15;
+pub mod event22;
 
 use std::error::Error;
 use aoc_rust::answer::Answer;
@@ -6,9 +7,13 @@ use aoc_rust::answer::Answer;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut answers: Vec<Answer> = Vec::new();
 
+    // 2015
     answers.push(event15::day01::solve()?);
     answers.push(event15::day02::solve()?);
     answers.push(event15::day03::solve()?);
+
+    // 2022
+    answers.push(event22::day01::solve()?);
 
     println!("+{:->39}+", '-');
     println!("| {:<5} | {:<3} | {:<10} | {:<10} |", "event", "day", "part 1", "part 2");
